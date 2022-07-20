a8bf31f1578261bd2060ba69bf21478bc4aa72ab
Nguyen Duong <muoi07052001@gmail.com>
2022-07-20 15:36:40 +0700

    Update return type hash password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import AppResponse from 'src/common/models/AppResponse';
+import { ServiceReturn } from 'src/common/models/ServiceReturn';
 import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
 import { comparePassword } from 'src/utils/bcrypt';
+import {
+  HTTP_EXCEPTION_ERROR_CODE,
+  HTTP_EXCEPTION_ERROR_MESSAGES,
+} from 'src/utils/constants';
 
 @Injectable()
 export class AuthService {
+  private readonly logger = new Logger(AuthService.name);
   constructor(
     private usersService: UsersService,
     private jwtService: JwtService,
   ) {}
 
-  async validateUser(username: string, password: string): Promise<any> {
+  async validateUser(
+    username: string,
+    password: string,
+  ): Promise<ServiceReturn<any>> {
     const user = await this.usersService.findOneByUsername(username);
 
     if (user.data) {
@@ -20,17 +30,33 @@ export class AuthService {
       if (comparePassword(password, salt, hashedPassword)) {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const { password, ...rest } = user.data as User;
-        return rest;
+        return {
+          err: null,
+          data: rest,
+        };
       } else {
-        console.log('Password is not valid');
-        return null;
+        this.logger.log(`Login error: Invalid password`);
+        return {
+          err: AppResponse.unauthorized(
+            [HTTP_EXCEPTION_ERROR_MESSAGES.INVALID_PASSWORD],
+            HTTP_EXCEPTION_ERROR_CODE.INVALID_PASSWORD,
+          ),
+          data: null,
+        };
       }
     } else {
-      console.log('Username does not exist');
+      this.logger.log(`Login error: Not found user`);
+      return {
+        err: AppResponse.notFound(
+          [HTTP_EXCEPTION_ERROR_MESSAGES.USER_DOES_NOT_EXIST],
+          HTTP_EXCEPTION_ERROR_CODE.USER_DOES_NOT_EXIST,
+        ),
+        data: null,
+      };
     }
   }
 
-  async login(user: User) {
+  async login(user: User): Promise<any> {
     const payload = { username: user.username, sub: user.id };
 
     return {
